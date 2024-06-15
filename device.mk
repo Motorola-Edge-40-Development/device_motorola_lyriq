134843547dfdb5d73fe877ce284cea86b5fc2323
Roshan Pius <rpius@google.com>
2024-06-15 12:12:48 +0200

    lyriq: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Id8440726f9a523a2afbb796cced8e2bd2876d230
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- device.mk
+++ device.mk
@@ -1,3 +1,4 @@
+
 #
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
@@ -127,6 +128,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
+# overlays
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay \
+    WifiOverlay
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
