c3473f96309776a3343ea03d2c2a1dcf2d9e3b9b
sabrina010209 <sebidk700@gmail.com>
2025-07-01 19:18:48 -0500

    lyriq: Define arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,18 @@ TARGET_NO_BOOTLOADER := true
 # Platform
 TARGET_BOARD_PLATFORM := mt6893
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a78
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
 # Inherit the proprietary files
 include vendor/motorola/lyriq/BoardConfigVendor.mk
