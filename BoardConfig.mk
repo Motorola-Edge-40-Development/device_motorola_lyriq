c71b232d253b885ee84ca63f1cd272e5e65dcb53
Notganesh <chimeports@gmail.com>
2026-03-18 18:53:50 +0530

    lyriq: initial device tree
    Signed-off-by: Notganesh <chimeports@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/lyriq
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lyriq
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6893
+
+# Inherit the proprietary files
+include vendor/motorola/lyriq/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/motorola/lyriq/lyriq-vendor.mk)
